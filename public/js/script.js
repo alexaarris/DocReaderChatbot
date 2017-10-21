3e6944f94f94148d657d08fdfeed85b5db185fec
alexaarris <32832652+alexaarris@users.noreply.github.com>
2017-10-21 06:32:27 +0200

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -45,8 +45,9 @@ function synthVoice(text) {
   const utterance = new SpeechSynthesisUtterance();
   const voices = window.speechSynthesis.getVoices();
   utterance.text = text;
-  utterance.rate = 1.8;
-  utterance.name = 'Google UK English Female';
+  utterance.rate = 1.8;  
+  utterance.voice = voices[10];
+  utterance.voiceURI = 'Google UK English Female';
   synth.speak(utterance);
 }
 
