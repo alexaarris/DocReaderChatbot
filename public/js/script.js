511b60d6467196f71b77eda7af21da498c7cf238
alexaarris <32832652+alexaarris@users.noreply.github.com>
2017-10-23 11:24:50 +0200

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -45,7 +45,7 @@ function synthVoice(text) {
   const utterance = new SpeechSynthesisUtterance();
   const voices = window.speechSynthesis.getVoices();
   utterance.text = text;
-  utterance.rate = 3;
+  utterance.rate = 2;
   synth.speak(utterance);
 }
 
